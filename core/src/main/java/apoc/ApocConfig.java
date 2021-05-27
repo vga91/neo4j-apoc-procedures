bf8ee2b66651a8112c573ae80e190d39dbde40db
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2021-05-27 17:15:34 +0200

    Bug in CALL apoc.metrics.storage('dbms.directories.data') due to APOC configuration check
    Fixes #1829

--- core/src/main/java/apoc/ApocConfig.java
+++ core/src/main/java/apoc/ApocConfig.java
@@ -30,6 +30,8 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static apoc.util.FileUtils.isFile;
+import static org.neo4j.configuration.BootloaderSettings.lib_directory;
+import static org.neo4j.configuration.BootloaderSettings.run_directory;
 import static org.neo4j.configuration.GraphDatabaseSettings.SYSTEM_DATABASE_NAME;
 import static org.neo4j.configuration.GraphDatabaseSettings.data_directory;
 import static org.neo4j.configuration.GraphDatabaseSettings.load_csv_file_url_root;
@@ -75,6 +77,8 @@ public class ApocConfig extends LifecycleAdapter {
             logs_directory,
             plugin_dir,
             transaction_logs_root_path,
+            run_directory,
+            lib_directory,
             neo4j_home
     ));
     private static final String DEFAULT_PATH = ".";
