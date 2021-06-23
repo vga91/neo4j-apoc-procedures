c85c6e59b29b194384b42fc6b6c97855fc6916f8
vga91 <giuseppe.villani@larus-ba.it>
2021-06-23 15:31:46 +0200

    Fixes #1168: add a version of periodic.iterate streaming that returns one row per batch

--- core/src/main/java/apoc/periodic/Periodic.java
+++ core/src/main/java/apoc/periodic/Periodic.java
@@ -230,6 +230,17 @@ public class Periodic {
         };
     }
 
+    @Procedure(mode = Mode.WRITE, name = "apoc.periodic.iterate.stream")
+    @Description("apoc.periodic.iterate.stream('statement returning items', 'statement per item', $config) - Same as the apoc.periodic.iterate, but returns a result for each batch (with the `batchNo` field indicating the batch number instead of `batches` field)")
+    public Stream<CurrentBatchResult> iterateStream(
+            @Name("cypherIterate") String cypherIterate,
+            @Name("cypherAction") String cypherAction,
+            @Name("config") Map<String,Object> config) {
+        return getTotalBatchResultStream(cypherIterate, cypherAction, config, true)
+                .map(CurrentBatchResult.class::cast);
+    }
+            
+
     /**
      * Invoke cypherAction in batched transactions being fed from cypherIteration running in main thread
      * @param cypherIterate
@@ -241,6 +252,11 @@ public class Periodic {
             @Name("cypherIterate") String cypherIterate,
             @Name("cypherAction") String cypherAction,
             @Name("config") Map<String,Object> config) {
+        return getTotalBatchResultStream(cypherIterate, cypherAction, config, false)
+                .map(BatchAndTotalResult.class::cast);
+    }
+
+    private Stream<BatchResultBase> getTotalBatchResultStream(String cypherIterate, String cypherAction, Map<String, Object> config, boolean stream) {
         validateQuery(cypherIterate);
 
         long batchSize = Util.toLong(config.getOrDefault("batchSize", 10000));
@@ -259,19 +275,19 @@ public class Periodic {
         Map<String,Object> params = (Map<String, Object>) config.getOrDefault("params", Collections.emptyMap());
 
         try (Result result = tx.execute(slottedRuntime(cypherIterate),params)) {
-            Pair<String,Boolean> prepared = PeriodicUtils.prepareInnerStatement(cypherAction, batchMode, result.columns(), "_batch");
+            Pair<String, Boolean> prepared = PeriodicUtils.prepareInnerStatement(cypherAction, batchMode, result.columns(), "_batch");
             String innerStatement = prepared.first();
             boolean iterateList = prepared.other();
-            log.info("starting batching from `%s` operation using iteration `%s` in separate thread", cypherIterate,cypherAction);
+            log.info("starting batching from `%s` operation using iteration `%s` in separate thread", cypherIterate, cypherAction);
             return PeriodicUtils.iterateAndExecuteBatchedInSeparateThread(
                     db, terminationGuard, log, pools,
-                    (int)batchSize, parallel, iterateList, retries, result,
+                    (int) batchSize, parallel, iterateList, retries, result,
                     (tx, p) -> {
                         final Result r = tx.execute(innerStatement, merge(params, p));
                         Iterators.count(r); // XXX: consume all results
                         return r.getQueryStatistics();
                     },
-                    concurrency, failedParams);
+                    concurrency, failedParams, stream);
         }
     }
 
