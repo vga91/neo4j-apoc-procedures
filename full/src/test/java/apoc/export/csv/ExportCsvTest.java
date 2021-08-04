06044c50e83f57e39d8ff5e8419efcad5d4ce0d3
vga91 <giuseppe.villani@larus-ba.it>
2021-08-04 13:08:26 +0200

    changes rebase

--- full/src/test/java/apoc/export/csv/ExportCsvTest.java
+++ full/src/test/java/apoc/export/csv/ExportCsvTest.java
@@ -43,8 +43,6 @@ public class ExportCsvTest {
             "\"5\",\":Address\",\"\",\"\",\"\",\"\",\"\",\"via Benni\",,,%n" +
             ",,,,,,,,\"0\",\"1\",\"KNOWS\"%n" +
             ",,,,,,,,\"3\",\"4\",\"NEXT_DELIVERY\"%n");
-    
-    private static final String LZ4_EXT = ".lz4";
 
     private static File directory = new File("target/import");
     static { //noinspection ResultOfMethodCallIgnored
@@ -77,17 +75,17 @@ public class ExportCsvTest {
     public void testExportAllCsvHDFS() throws Exception {
         assertHdfsFile(NONE);
     }
-    
+
     @Test
     public void testExportAllCsvHDFSCompressed() {
         assertHdfsFile(BLOCK_LZ4);
     }
 
     private void assertHdfsFile(CompressionAlgo compression) {
-        String fileExt = compression.equals(NONE) ? "" : LZ4_EXT;
-        String hdfsUrl = String.format("hdfs://localhost:12345/user/%s/all.csv" + fileExt, System.getProperty("user.name"));
-        TestUtil.testCall(db, "CALL apoc.export.csv.all($file, $config)", 
-                map("file", hdfsUrl, "config", map("compression", compression.name())),
+        String fileExt = compression.equals(NONE) ? "" : ".lz4";
+        String hdfsUrl = String.format("%s/user/%s/all.csv", miniDFSCluster.getURI().toString(), System.getProperty("user.name"));
+        TestUtil.testCall(db, "CALL apoc.export.csv.all($file, $config)",
+                map("file", hdfsUrl + fileExt, "config", map("compression", compression.name())),
                 (r) -> {
                     try {
                         FileSystem fs = miniDFSCluster.getFileSystem();
