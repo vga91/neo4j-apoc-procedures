dbd715bf29f948e1c93b20f80b692ac992749495
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2021-09-20 12:11:31 +0200

    Update CypherTest.java

--- core/src/test/java/apoc/cypher/CypherTest.java
+++ core/src/test/java/apoc/cypher/CypherTest.java
@@ -63,7 +63,7 @@ public class CypherTest {
 
     @BeforeClass
     public static void setUp() {
-        apocConfig().setProperty(APOC_IMPORT_FILE_ENABLED, false);
+        apocConfig().setProperty(APOC_IMPORT_FILE_ENABLED, true);
         TestUtil.registerProcedure(db, Cypher.class, Utils.class, CypherFunctions.class, Timeboxed.class, Strings.class);
     }
 
