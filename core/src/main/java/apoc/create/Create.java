68ce9d7103cecee2c1b19953314bef8c0c1a2111
vga91 <giuseppe.villani@larus-ba.it>
2021-11-04 11:21:10 +0100

    changed VirtualNode id implementation - added NlpUtils

--- core/src/main/java/apoc/create/Create.java
+++ core/src/main/java/apoc/create/Create.java
@@ -159,7 +159,7 @@ public class Create {
     @UserFunction("apoc.create.virtual.fromNode")
     @Description("apoc.create.virtual.fromNode(node, [propertyNames]) returns a virtual node built from an existing node with only the requested properties")
     public Node virtualFromNodeFunction(@Name("node") Node node, @Name("propertyNames") List<String> propertyNames) {
-        return new VirtualNode(node, propertyNames, false);
+        return new VirtualNode(node, propertyNames);
     }
 
     @Procedure
@@ -236,7 +236,7 @@ public class Create {
     private PathResult createVirtualPath(Path path) {
         final Iterable<Relationship> relationships = path.relationships();
         final Node first = path.startNode();
-        VirtualPath virtualPath = new VirtualPath(new VirtualNode(first, Iterables.asList(first.getPropertyKeys()), false));
+        VirtualPath virtualPath = new VirtualPath(new VirtualNode(first, Iterables.asList(first.getPropertyKeys())));
         for (Relationship rel : relationships) {
             VirtualNode start = VirtualNode.from(rel.getStartNode());
             VirtualNode end = VirtualNode.from(rel.getEndNode());
