f5e4f47548a488f5dd38974b9603bdac686c8542
vga91 <giuseppe.villani@larus-ba.it>
2022-01-05 09:41:42 +0100

    reset Mapping.java to test ci

--- core/src/main/java/apoc/load/Mapping.java
+++ core/src/main/java/apoc/load/Mapping.java
@@ -2,13 +2,22 @@ package apoc.load;
 
 import apoc.load.util.LoadCsvConfig;
 import apoc.meta.Meta;
+import apoc.util.Util;
+import org.neo4j.values.storable.DateTimeValue;
+import org.neo4j.values.storable.DateValue;
+import org.neo4j.values.storable.DurationValue;
+import org.neo4j.values.storable.LocalDateTimeValue;
+import org.neo4j.values.storable.LocalTimeValue;
+import org.neo4j.values.storable.TimeValue;
 
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -17,30 +26,29 @@ import static apoc.util.DateParseUtil.DB_TEMPORAL_TIMEZONE;
 import static apoc.util.DateParseUtil.getTimezoneIfValid;
 import static apoc.util.Util.parseCharFromConfig;
 import static java.util.Collections.emptyList;
+import static org.neo4j.configuration.GraphDatabaseSettings.db_temporal_timezone;
 
-
-public class Mapping extends AbstractMapping {
-    public static final Mapping EMPTY = new Mapping("", Collections.emptyMap(), LoadCsvConfig.DEFAULT_ARRAY_SEP, false, null);
+public class Mapping {
+    public static final Mapping EMPTY = new Mapping("", Collections.emptyMap(), LoadCsvConfig.DEFAULT_ARRAY_SEP, false);
+    final String name;
+    final Collection<String> nullValues;
+    final Meta.Types type;
     final boolean array;
-    
+    final boolean ignore;
     final char arraySep;
     private final Pattern arrayPattern;
-    
-    public Mapping(String name, Map<String, Object> mapping, char arraySep, boolean ignore, ZoneId zoneId) {
-        super(name, mapping, ignore, emptyList(), zoneId);
+    private final Map<String, Object> optionalData;
+
+    public Mapping(String name, Map<String, Object> mapping, char arraySep, boolean ignore) {
+        this.name = mapping.getOrDefault("name", name).toString();
         this.array = (Boolean) mapping.getOrDefault("array", false);
+        this.optionalData = (Map<String, Object>) mapping.get("optionalData");
+
+        this.ignore = (Boolean) mapping.getOrDefault("ignore", ignore);
+        this.nullValues = (Collection<String>) mapping.getOrDefault("nullValues", emptyList());
         this.arraySep = parseCharFromConfig(mapping, "arraySep", arraySep);
+        this.type = Meta.Types.from(mapping.getOrDefault("type", "STRING").toString());
         this.arrayPattern = Pattern.compile(String.valueOf(this.arraySep), Pattern.LITERAL);
-        
-        this.listSupplier = value -> Arrays.stream(arrayPattern.split((String) value)).map(this::convertType).collect(Collectors.toList());
-        
-        if (this.zoneId == null) {
-            System.out.println("DB_TEMPORAL_TIMEZONE" + DB_TEMPORAL_TIMEZONE);
-            System.out.println("config here" +  apocConfig().getConfig());
-            // to preserve ImportCsv behavior like neo4j-import-tool
-            // we leverage on optionalData, e.g. myProp:time{timezone:+02:00}
-            this.zoneId = getTimezoneIfValid(optionalData, DB_TEMPORAL_TIMEZONE);
-        }
 
         if (this.type == null) {
             // Call this out to the user explicitly because deep inside of LoadCSV and others you will get
@@ -50,9 +58,8 @@ public class Mapping extends AbstractMapping {
         }
     }
 
-    public Object convert(Object value) {
-        final String stringValue = (String) value;
-        return array ? convertArray(stringValue) : convertType(stringValue);
+    public Object convert(String value) {
+        return array ? convertArray(value) : convertType(value);
     }
 
     private Object convertArray(String value) {
@@ -63,9 +70,44 @@ public class Mapping extends AbstractMapping {
         }
         return result;
     }
-    
+
     private Object convertType(String value) {
+        if (nullValues.contains(value)) return null;
         if (type == Meta.Types.STRING) return value;
-        return super.commonConvertType(value);
+
+//        System.out.println("timezone -- ");
+//        try {
+//            System.out.println("(String) optionalData.getOrDefault(timezone apocConfig().getString(db_temporal_timezone.name())) " + (String) optionalData.getOrDefault("timezone", apocConfig().getString(db_temporal_timezone.name())));
+//        } catch (NullPointerException e) {
+//            System.out.println("Mapping.convertType");
+//        }
+//        System.out.println("getTimezoneIfValid(optionalData, DB_TEMPORAL_TIMEZONE) " + getTimezoneIfValid(optionalData, DB_TEMPORAL_TIMEZONE));
+//        System.out.println("timezone end-- ");
+        
+        final Supplier<ZoneId> timezone = () -> ZoneId.of((String) optionalData.getOrDefault("timezone", apocConfig().getString(db_temporal_timezone.name())));
+        switch (type) {
+            case POINT:
+                return Util.toPoint(Util.fromJson(value, Map.class), optionalData);
+            case LOCAL_DATE_TIME:
+                // asObjectCopy() returns LocalDateTime, 
+                // because in case of array entity.setProperty() fails with LocalDateTimeValue[]
+                return LocalDateTimeValue.parse(value).asObjectCopy();
+            case LOCAL_TIME:
+                return LocalTimeValue.parse(value).asObjectCopy();
+            case DATE_TIME:
+                return DateTimeValue.parse(value, timezone).asObjectCopy();
+            case TIME:
+                return TimeValue.parse(value, timezone).asObjectCopy();
+            case DATE:
+                return DateValue.parse(value).asObjectCopy();
+            case DURATION:
+                return DurationValue.parse(value);
+            case INTEGER: return Util.toLong(value);
+            case FLOAT: return Util.toDouble(value);
+            case BOOLEAN: return Util.toBoolean(value);
+            case NULL: return null;
+            case LIST: return Arrays.stream(arrayPattern.split(value)).map(this::convertType).collect(Collectors.toList());
+            default: return value;
+        }
     }
 }
