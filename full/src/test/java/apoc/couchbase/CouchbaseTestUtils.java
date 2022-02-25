2610641b87909f7f4d25fbb3c46a85b0cd97cefb
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2022-02-25 17:54:37 +0100

    fix Couchbase in TC

--- full/src/test/java/apoc/couchbase/CouchbaseTestUtils.java
+++ full/src/test/java/apoc/couchbase/CouchbaseTestUtils.java
@@ -22,6 +22,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
