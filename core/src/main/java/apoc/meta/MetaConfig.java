f48863be0d295eba481686142525a23d05de93ac
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2022-06-06 10:04:41 +0200

    small changes / adds

--- core/src/main/java/apoc/meta/MetaConfig.java
+++ core/src/main/java/apoc/meta/MetaConfig.java
@@ -38,8 +38,6 @@ public class MetaConfig {
 
         // To maintain backwards compatibility, need to still support "labels", "rels" and "excludes" for "includeLabels", "includeRels" and "excludeLabels" respectively.
 
-        // todo - controllare tutti i config con labels, rels, excludes ed in caso mettere anche test "non deprecati"
-        
         Set<String> includesLabelsLocal = new HashSet<>((Collection<String>)config.getOrDefault("labels",Collections.EMPTY_SET));
         Set<String> includesRelsLocal = new HashSet<>((Collection<String>)config.getOrDefault("rels",Collections.EMPTY_SET));
         Set<String> excludesLocal = new HashSet<>((Collection<String>)config.getOrDefault("excludes",Collections.EMPTY_SET));
@@ -78,7 +76,7 @@ public class MetaConfig {
     }
 
     public Set<String> getExcludeRels() {
-        return excludeRels; // todo - manca adoc e tests..
+        return excludeRels;
     }
 
     public long getSample() {
@@ -86,7 +84,7 @@ public class MetaConfig {
     }
 
     public long getMaxRels() {
-        return maxRels; // todo, mancano test e adoc ..., almeno in meta.graph, ma penso anche in altri...
+        return maxRels;
     }
 
     /**
