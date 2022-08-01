05bd3c531ecca689e63ba4181bdf8a4919037bd9
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2022-08-01 12:32:17 +0200

    changes review

--- core/src/main/java/apoc/export/cypher/FileManagerFactory.java
+++ core/src/main/java/apoc/export/cypher/FileManagerFactory.java
@@ -2,8 +2,8 @@ package apoc.export.cypher;
 
 import java.io.OutputStream;
 import apoc.export.util.ExportConfig;
-import apoc.util.CompressionAlgo;
 import apoc.util.Util;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.PrintWriter;
@@ -31,31 +31,8 @@ public class FileManagerFactory {
         }
         fileName = fileName.trim();
 
-        final CompressionAlgo compressionAlgo = CompressionAlgo.valueOf(config.getCompressionAlgo());
-        int indexOfDot = getIndexOfDot(fileName, compressionAlgo);
-        String fileType = indexOfDot == StringUtils.INDEX_NOT_FOUND 
-                ? null 
-                : fileName.substring(indexOfDot + 1);
+        String fileType = FilenameUtils.getExtension(fileName);
         return new PhysicalExportFileManager(fileType, fileName, separatedFiles, config);
-    }
-
-    private static int getIndexOfDot(String fileName, CompressionAlgo compressionAlgo) {
-        final int i = StringUtils.countMatches(fileName, DOT);
-        // if file is without dot, we leave the file name as is, just adding e.g `nodes.LABELNAME` in case of `bulkImport: true`
-        if (i == 0) {
-            return StringUtils.INDEX_NOT_FOUND;
-        }
-        
-        // In case of export with separated files (with bulkImport: true) in case of compressed file retrieve, if present, the compression extension too
-        // So, if with 2 dot, in case of compressed, we consider the latest 2 part, e.g ".csv.gz" as the final ext
-        // For example in case of file test.one.two.csv.gz we will export test.one.two.nodes.LABELNAME.csv.gz, test.one.two.relationships.RELTYPE.csv.gz, etc...
-        // Otherwise, in case of file with 1 dot, we consider only last part as extension (for both compressed and node),
-        // so, a file "test.foo", will export "test.bulkImport.LABELNAME.foo", etc... in case of `bulkImport`
-        int ordinal = compressionAlgo.isNone() || i == 1 
-                ? 1 
-                : 2;
-
-        return StringUtils.lastOrdinalIndexOf(fileName, DOT, ordinal);
     }
 
     private static class PhysicalExportFileManager implements ExportFileManager {
@@ -67,7 +44,7 @@ public class FileManagerFactory {
         private ExportConfig config;
 
         public PhysicalExportFileManager(String fileType, String fileName, boolean separatedFiles, ExportConfig config) {
-            this.fileType = fileType;
+            this.fileType = StringUtils.isBlank(fileType) ? "" : fileType;
             this.fileName = fileName;
             this.separatedFiles = separatedFiles;
             this.config = config;
@@ -89,17 +66,15 @@ public class FileManagerFactory {
         }
 
         private String normalizeFileName(final String fileName, String suffix) {
-            if (suffix == null) {
+            if (StringUtils.isBlank(suffix)) {
                 return fileName;
             }
-            final String dotSuffix = DOT + suffix;
             // in case of file without dot extension, we just add the suffix 
             if (StringUtils.isEmpty(fileType)) {
-                return fileName + dotSuffix;
+                return fileName + DOT + suffix;
             }
-            final String dotType = DOT + fileType;
             // TODO check if this should be follow the same rules of FileUtils.readerFor
-            return fileName.replace(dotType, dotSuffix + dotType);
+            return fileName.replace(fileType, suffix + DOT + fileType);
         }
 
         @Override
--- core/src/test/java/apoc/export/csv/ExportCsvNeo4jAdminTest.java
+++ core/src/test/java/apoc/export/csv/ExportCsvNeo4jAdminTest.java
@@ -7,15 +7,19 @@ import apoc.util.CompressionAlgo;
 import apoc.util.TestUtil;
 import apoc.util.Util;
 import org.apache.commons.lang.exception.ExceptionUtils;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.neo4j.configuration.GraphDatabaseSettings;
+import org.neo4j.internal.helpers.collection.Iterators;
 import org.neo4j.test.rule.DbmsRule;
 import org.neo4j.test.rule.ImpermanentDbmsRule;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -80,13 +84,13 @@ public class ExportCsvNeo4jAdminTest {
         directory.mkdirs();
     }
 
-    @ClassRule
-    public static DbmsRule db = new ImpermanentDbmsRule()
+    @Rule
+    public DbmsRule db = new ImpermanentDbmsRule()
             .withSetting(GraphDatabaseSettings.load_csv_file_url_root, directory.toPath().toAbsolutePath())
             .withSetting(ApocSettings.apoc_export_file_enabled, true);
 
-    @BeforeClass
-    public static void setUp() throws Exception {
+    @Before
+    public void before() throws Exception {
         TestUtil.registerProcedure(db, ExportCSV.class, Graphs.class);
         db.executeTransactionally("CREATE (f:User1:User {name:'foo',age:42,male:true,kids:['a','b','c']})-[:KNOWS]->(b:User {name:'bar',age:42}),(c:User {age:12})");
         db.executeTransactionally("CREATE (f:Address1:Address {name:'Andrea', city: 'Milano', street:'Via Garibaldi, 7'})-[:NEXT_DELIVERY]->(a:Address {name: 'Bar Sport'}), (b:Address {street: 'via Benni'})");
@@ -208,11 +212,11 @@ public class ExportCsvNeo4jAdminTest {
         assertFileEquals(file,EXPECTED_NEO4J_ADMIN_IMPORT_HEADER_RELATIONSHIP_NEXT_DELIVERY + EXPECTED_NEO4J_ADMIN_IMPORT_RELATIONSHIP_NEXT_DELIVERY, fileBaseName + ".relationships.NEXT_DELIVERY" + fileExpectedExt, separator);
     }
 
-    public static void assertFileEquals(String base, String expected, String file) {
+    private static void assertFileEquals(String base, String expected, String file) {
         assertFileEquals(base, expected, file, ",",  CompressionAlgo.NONE);
     }
 
-    public static void assertFileEquals(String base, String expected, String file, CompressionAlgo algo) {
+    private static void assertFileEquals(String base, String expected, String file, CompressionAlgo algo) {
         assertFileEquals(base, expected, file, ",",  algo);
     }
 
@@ -266,6 +270,44 @@ public class ExportCsvNeo4jAdminTest {
         assertEquals(fileName, r.get("file"));
         assertEquals("csv", r.get("format"));
         assertTrue("Should get time greater than 0",((long) r.get("time")) >= 0);
+    }
+
+    @Test
+    public void testExportCypherWithIdField() throws Exception {
+        // given
+        db.executeTransactionally("MATCH (n) DETACH DELETE n");
+        final Map<String, Object> map = db.executeTransactionally("CREATE (source:User:Larus{id: 1, name: 'Andrea'})-[:KNOWS{id: 10}]->(target:User:Neo4j{id: 2, name: 'Michael'})\n" +
+                "RETURN id(source) as sourceId, id(target) as targetId", Collections.emptyMap(), result ->  Iterators.single(result) );
+        final String fileName = "export_id_field";
+        String fileNameWithExtension = fileName + ".csv";
+        File dir = new File(directory, fileNameWithExtension);
+
+        // when
+        TestUtil.testCall(db, "CALL apoc.export.csv.all($fileNameWithExtension,{bulkImport: true})",
+                map("fileNameWithExtension", fileNameWithExtension), r -> {
+                    // then
+                    assertEquals(20000L, r.get("batchSize"));
+                    assertEquals(1L, r.get("batches"));
+                    assertEquals(2L, r.get("nodes"));
+                    assertEquals(3L, r.get("rows"));
+                    assertEquals(1L, r.get("relationships"));
+                    assertEquals(5L, r.get("properties"));
+                    assertTrue("Should get time greater than 0",
+                            ((long) r.get("time")) >= 0);
+
+                    String file = dir.getParent() + File.separator;
+                    String expectedNodesLarus = String.format(":ID,name,id:long,:LABEL%n"
+                            + "%s,Andrea,1,User;Larus%n", map.get("sourceId"));
+                    String expectedNodesNeo4j = String.format(":ID,name,id:long,:LABEL%n"
+                            +"%s,Michael,2,User;Neo4j%n", map.get("targetId"));
+                    String expectedRelsNeo4j = String.format(":START_ID,:END_ID,:TYPE,id:long%n"
+                            + "%s,%s,KNOWS,10%n", map.get("sourceId"), map.get("targetId"));
+
+                    assertFileEquals(file, expectedNodesLarus, fileName + ".nodes.User.Larus.csv");
+                    assertFileEquals(file, expectedNodesNeo4j, fileName + ".nodes.User.Neo4j.csv");
+                    assertFileEquals(file, expectedRelsNeo4j, fileName + ".relationships.KNOWS.csv");
+                }
+        );
     }
 }
 
