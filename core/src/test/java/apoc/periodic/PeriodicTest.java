848cba648bea79ba24b34d69b253bef3403c920f
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2022-08-02 16:14:50 +0200

    removed auto-gen doc

--- core/src/test/java/apoc/periodic/PeriodicTest.java
+++ core/src/test/java/apoc/periodic/PeriodicTest.java
@@ -45,7 +45,6 @@ import static java.util.stream.StreamSupport.stream;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
