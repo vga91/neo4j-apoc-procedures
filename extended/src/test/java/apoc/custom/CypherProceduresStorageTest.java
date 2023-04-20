95d6e3430bb20088540ab26459ea18de8ffc00eb
vga91 <giuseppe.villani@larus-ba.it>
2023-04-20 10:11:46 +0200

    [6R2yEhGU] removed unnecessary clear caches

--- extended/src/test/java/apoc/custom/CypherProceduresStorageTest.java
+++ extended/src/test/java/apoc/custom/CypherProceduresStorageTest.java
@@ -624,8 +624,7 @@ public class CypherProceduresStorageTest {
             );
         });
 
-        // check that the previous overwrite works correctly after the `db.clearQueryCaches`
-        db.executeTransactionally("call db.clearQueryCaches");
+        // check that the previous overwrite works correctly
         listProcNames.forEach(name -> TestUtil.testCall(db,
                         String.format("call custom.%s", name),
                         (row) -> assertEquals(1L, row.get("answer"))
@@ -671,8 +670,7 @@ public class CypherProceduresStorageTest {
             );
         });
 
-        // check that the previous overwrite works correctly after the `db.clearQueryCaches`
-        db.executeTransactionally("call db.clearQueryCaches");
+        // check that the previous overwrite works correctly
         listFunNames.forEach(name -> TestUtil.testCall(db,
                         String.format(funQuery, name),
                         (row) -> assertEquals(1L, row.get("row"))
