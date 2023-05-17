71fb47fb99e4cea0f675c5b676e9d05f140c156d
vga91 <giuseppe.villani@larus-ba.it>
2023-05-17 09:10:02 +0200

    var additions

--- extended/src/main/java/apoc/export/parquet/ExportParquetResultFileStrategy.java
+++ extended/src/main/java/apoc/export/parquet/ExportParquetResultFileStrategy.java
@@ -5,7 +5,10 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.logging.Log;
 import org.neo4j.procedure.TerminationGuard;
 
-public class ExportParquetResultFileStrategy implements ExportParquetFileStrategy {
+public class ExportParquetResultFileStrategy extends ExportParquetFileStrategy {
     public ExportParquetResultFileStrategy(String fileName, GraphDatabaseService db, Pools pools, TerminationGuard terminationGuard, Log logger) {
+        super(fileName, db, pools, terminationGuard, logger);
     }
+//    public ExportParquetResultFileStrategy(String fileName, GraphDatabaseService db, Pools pools, TerminationGuard terminationGuard, Log logger) {
+//    }
 }
--- extended/src/main/java/apoc/export/parquet/ParquetConfig.java
+++ extended/src/main/java/apoc/export/parquet/ParquetConfig.java
@@ -1,8 +1,31 @@
 package apoc.export.parquet;
 
+import apoc.util.Util;
+
+import java.util.Collections;
+import java.util.Map;
+
 public class ParquetConfig {
     // todo - extends ExportConfig ??
 
     // -- todo -->  .withWriteMode(ParquetFileWriter.Mode.OVERWRITE)
+
+    private final int batchSize;
+
+    private final Map<String, Object> config;
+
+    public ParquetConfig(Map<String, Object> config) {
+        this.config = config == null ? Collections.emptyMap() : config;
+        this.batchSize = Util.toInteger(this.config.getOrDefault("batchSize", 2000));
+    }
+
+    public int getBatchSize() {
+        return batchSize;
+    }
+
+    // todo - useful??
+    public Map<String, Object> getConfig() {
+        return config;
+    }
 }
 
