f36e5438c60919b7029cc82a2016daadeddd11be
vga91 <giuseppe.villani@larus-ba.it>
2023-08-29 16:33:27 +0200

    added deprecations

--- extended/src/main/java/apoc/custom/CypherProcedures.java
+++ extended/src/main/java/apoc/custom/CypherProcedures.java
@@ -34,6 +34,7 @@ import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 import static apoc.custom.CypherProceduresHandler.*;
+import static apoc.util.SystemDbUtil.checkWriteAllowed;
 
 /**
  * @author mh
@@ -41,6 +42,10 @@ import static apoc.custom.CypherProceduresHandler.*;
  */
 @Extended
 public class CypherProcedures {
+    private static final String MSG_DEPRECATION = """
+            Please note that the current procedure is deprecated,
+            it's recommended to use the `apoc.custom.installProcedure`, `apoc.custom.installFunction`, `apoc.uuid.dropProcedure` , `apoc.uuid.dropFunction` , `apoc.uuid.dropAll` procedures executed against the 'system' database
+            instead of, respectively, `apoc.uuid.declareProcedure`, `apoc.uuid.declareFunction`, `apoc.custom.removeProcedure`, `apoc.custom.removeFunction`, `apoc.custom.removeAll`.""";
 
     // visible for testing
     public static final String ERROR_MISMATCHED_INPUTS = "Required query parameters do not match provided input arguments.";
@@ -55,12 +60,14 @@ public class CypherProcedures {
     @Context
     public CypherProceduresHandler cypherProceduresHandler;
 
-    @Procedure(value = "apoc.custom.declareProcedure", mode = Mode.WRITE)
+    @Deprecated
+    @Procedure(value = "apoc.custom.declareProcedure", mode = Mode.WRITE, deprecatedBy = "apoc.custom.installProcedure")
     @Description("apoc.custom.declareProcedure(signature, statement, mode, description) - register a custom cypher procedure")
     public void declareProcedure(@Name("signature") String signature, @Name("statement") String statement,
                                  @Name(value = "mode", defaultValue = "read") String mode,
                                  @Name(value = "description", defaultValue = "") String description
     ) {
+        checkWriteAllowed(MSG_DEPRECATION);
         Mode modeProcedure = cypherProceduresHandler.mode(mode);
         ProcedureSignature procedureSignature = new Signatures(PREFIX).asProcedureSignature(signature, description, modeProcedure);
         validateProcedure(statement, procedureSignature.inputSignature(), procedureSignature.outputSignature(), modeProcedure);
@@ -68,11 +75,13 @@ public class CypherProcedures {
         cypherProceduresHandler.storeProcedure(procedureSignature, statement);
     }
 
-    @Procedure(value = "apoc.custom.declareFunction", mode = Mode.WRITE)
+    @Deprecated
+    @Procedure(value = "apoc.custom.declareFunction", mode = Mode.WRITE, deprecatedBy = "apoc.custom.installFunction")
     @Description("apoc.custom.declareFunction(signature, statement, forceSingle, description) - register a custom cypher function")
     public void declareFunction(@Name("signature") String signature, @Name("statement") String statement,
                            @Name(value = "forceSingle", defaultValue = "false") boolean forceSingle,
                            @Name(value = "description", defaultValue = "") String description) throws ProcedureException {
+        checkWriteAllowed(MSG_DEPRECATION);
         UserFunctionSignature userFunctionSignature = new Signatures(PREFIX).asFunctionSignature(signature, description);
         final Signatures signatures = new Signatures(PREFIX);
         final SignatureParser.FunctionContext functionContext = signatures.parseFunction(signature);
@@ -101,17 +110,21 @@ public class CypherProcedures {
     }
     
 
-    @Procedure(value = "apoc.custom.removeProcedure", mode = Mode.WRITE)
+    @Deprecated
+    @Procedure(value = "apoc.custom.removeProcedure", mode = Mode.WRITE, deprecatedBy = "apoc.custom.dropProcedure")
     @Description("apoc.custom.removeProcedure(name) - remove the targeted custom procedure")
     public void removeProcedure(@Name("name") String name) {
+        checkWriteAllowed(MSG_DEPRECATION);
         Objects.requireNonNull(name, "name");
         cypherProceduresHandler.removeProcedure(name);
     }
 
 
-    @Procedure(value = "apoc.custom.removeFunction", mode = Mode.WRITE)
+    @Deprecated
+    @Procedure(value = "apoc.custom.removeFunction", mode = Mode.WRITE, deprecatedBy = "apoc.custom.dropFunction")
     @Description("apoc.custom.removeFunction(name, type) - remove the targeted custom function")
     public void removeFunction(@Name("name") String name) {
+        checkWriteAllowed(MSG_DEPRECATION);
         Objects.requireNonNull(name, "name");
         cypherProceduresHandler.removeFunction(name);
     }
