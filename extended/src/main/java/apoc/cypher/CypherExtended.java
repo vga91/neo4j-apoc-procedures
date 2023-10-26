38f3908bede9be8be54d4c6d90eeaf5e4097a92d
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2023-10-26 12:14:49 +0200

    Fixes 3751: Failed to invoke procedure apoc.cypher.mapParallel2: Caused by:RuntimeException: Error polling, timeout

--- extended/src/main/java/apoc/cypher/CypherExtended.java
+++ extended/src/main/java/apoc/cypher/CypherExtended.java
@@ -178,7 +178,7 @@ public class CypherExtended {
                 if (isPeriodicOperation(stmt)) {
                     Util.inThread(pools , () -> {
                         try {
-                            return db.executeTransactionally(stmt, params, result -> consumeResult(result, queue, addStatistics, timeout));
+                            return db.executeTransactionally(stmt, params, result -> consumeResult(result, queue, addStatistics, tx));
                         } catch (Exception e) {
                             collectError(queue, reportError, e, fileName);
                             return null;
@@ -188,7 +188,7 @@ public class CypherExtended {
                 else {
                     Util.inTx(db, pools, threadTx -> {
                         try (Result result = threadTx.execute(stmt, params)) {
-                            return consumeResult(result, queue, addStatistics, timeout);
+                            return consumeResult(result, queue, addStatistics, tx);
                         } catch (Exception e) {
                             collectError(queue, reportError, e, fileName);
                             return null;
@@ -231,7 +231,7 @@ public class CypherExtended {
             if (schemaOperation) {
                 Util.inTx(db, pools, txInThread -> {
                     try (Result result = txInThread.execute(stmt, params)) {
-                        return consumeResult(result, queue, addStatistics, timeout);
+                        return consumeResult(result, queue, addStatistics, tx);
                     } catch (Exception e) {
                         collectError(queue, reportError, e, fileName);
                         return null;
@@ -243,13 +243,14 @@ public class CypherExtended {
 
     private final static Pattern shellControl = Pattern.compile("^:?\\b(begin|commit|rollback)\\b", Pattern.CASE_INSENSITIVE);
 
-    private Object consumeResult(Result result, BlockingQueue<RowResult> queue, boolean addStatistics, long timeout) {
+    private Object consumeResult(Result result, BlockingQueue<RowResult> queue, boolean addStatistics, Transaction tx) {
         try {
             long time = System.currentTimeMillis();
             int row = 0;
             while (result.hasNext()) {
                 terminationGuard.check();
                 Map<String, Object> res = EntityUtil.anyRebind(tx, result.next());
+//                Map<String, Object> res = tx == null ? result.next() : EntityUtil.anyRebind(tx, result.next());
                 queue.put(new RowResult(row++, res));
             }
             if (addStatistics) {
@@ -377,9 +378,12 @@ public class CypherExtended {
         Util.inFuture(pools, () -> {
             long total = parallelPartitions
                 .map((List<Object> partition) -> {
-                    try (Transaction transaction = db.beginTx();
-                         Result result = transaction.execute(statement, parallelParams(params, "_", partition))) {
-                        return consumeResult(result, queue, false, timeout);
+                    Transaction transaction = db.beginTx();
+                    Result result = transaction.execute(statement, parallelParams(params, "_", partition));
+                    try 
+//                                () 
+                    {
+                        return consumeResult(result, queue, false, transaction);
                     } catch (Exception e) {
                         throw new RuntimeException(e);
                     }}
@@ -387,6 +391,8 @@ public class CypherExtended {
             queue.put(RowResult.TOMBSTONE);
             return total;
         });
+        
+        
         return StreamSupport.stream(new QueueBasedSpliterator<>(queue, RowResult.TOMBSTONE, terminationGuard, (int)timeout),true).map((rowResult) -> new MapResult(rowResult.result));
     }
 
