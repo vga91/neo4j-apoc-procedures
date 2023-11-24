92d4b4e495b167857c99d8aff25eddc18fc295e0
Giuseppe Villani <giuseppe.villani@larus-ba.it>
2023-11-24 15:50:57 +0100

    Update ParquetTest.java

--- extended/src/test/java/apoc/export/parquet/ParquetTest.java
+++ extended/src/test/java/apoc/export/parquet/ParquetTest.java
@@ -48,7 +48,7 @@ public class ParquetTest {
     public static final Map<String, Map<String, String>> MAPPING_QUERY = Map.of("mapping",
             Map.of("n", "Node", "r", "Relationship", "o", "Node")
     );
-    private static File directory = new File("target/parquetImport");
+    private static File directory = new File("target/parquet import");
     static { //noinspection ResultOfMethodCallIgnored
         directory.mkdirs();
     }
