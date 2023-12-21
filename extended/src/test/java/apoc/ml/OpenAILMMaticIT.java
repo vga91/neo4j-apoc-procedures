ec5708ccd6f0b8edac68452dd0d285fc546bf18a
vga91 <giuseppe.villani@larus-ba.it>
2023-12-21 13:04:18 +0100

    improved handler

--- extended/src/test/java/apoc/ml/OpenAILMMaticIT.java
+++ extended/src/test/java/apoc/ml/OpenAILMMaticIT.java
@@ -5,24 +5,35 @@ import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.neo4j.test.assertion.Assert;
 import org.neo4j.test.rule.DbmsRule;
 import org.neo4j.test.rule.ImpermanentDbmsRule;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
+import static apoc.ApocConfig.apocConfig;
 import static apoc.ml.OpenAI.API_TYPE_CONF_KEY;
 import static apoc.ml.OpenAI.ENDPOINT_CONF_KEY;
 import static apoc.ml.OpenAITestResultUtils.assertChatCompletion;
 import static apoc.ml.OpenAITestResultUtils.assertCompletion;
 import static apoc.util.TestUtil.testCall;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/*
+To start the test, follow the instructions in this video: https://www.youtube.com/watch?v=V_baaAZMY44
+
+The APIs, especially the `/completions` one, are extremely unstable (i.e. we could get many SocketTimeoutExceptions),
+even via e.g. Insomnia,
+so it's better to change the `nTokPredict` value, placed in `llmatic.config.json`, to a low value, like `128`
+
+ */
 public class OpenAILMMaticIT {
-
-    /*
-    Follow the instructions TODO
-    */
+    public static final String MODEL_ID = "meta-llama/Llama-2-70b-chat-hf";
 
     private String localAIUrl;
 
@@ -32,42 +43,89 @@ public class OpenAILMMaticIT {
 
     @Before
     public void setUp() throws Exception {
+        apocConfig().setProperty("apoc.http.timeout.connect", 30_000);
+        apocConfig().setProperty("apoc.http.timeout.read", 30_000);
+        
         localAIUrl = System.getenv("LLM_MATIC_URL");
         Assume.assumeNotNull("No LOCAL_AI_URL environment configured", localAIUrl);
 //        openaiKey = System.getenv("OPENAI_KEY");
 //        Assume.assumeNotNull("No OPENAI_KEY environment configured", openaiKey);
         TestUtil.registerProcedure(db, OpenAI.class);
+        
     }
 
     @Test
     public void getEmbedding() {
-        testCall(db, "CALL apoc.ml.openai.embedding(['Some Text'], $apiKey, $conf)",
+        assertEventually(() -> db.executeTransactionally("CALL apoc.ml.openai.embedding(['Some Text'], $apiKey, $conf)",
                 getParams("thenlper/gte-large"),
-                row -> {
+                r -> {
+                    Map<String, Object> row = r.next();
                     assertEquals(0L, row.get("index"));
                     assertEquals("Some Text", row.get("text"));
                     var embedding = (List<Double>) row.get("embedding");
                     assertEquals(5120, embedding.size());
-                });
+                    assertFalse(r.hasNext());
+                    return true;
+                }
+        ));
     }
 
     @Test
     public void completion() {
-        testCall(db, "CALL apoc.ml.openai.completion('What color is the sky? Answer in one word: ', $apiKey, $conf)",
-                getParams("Meta-Llama/Llama-Guard-7b"),
-                (row) -> assertCompletion(row, "text-davinci-003"));
+        assertEventually(() -> db.executeTransactionally("CALL apoc.ml.openai.completion('What color is the sky? Answer in one word: ', $apiKey, $conf)",
+                getParams(MODEL_ID),
+                (r) -> {
+                    Map<String, Object> row = r.next();
+                    var result = (Map<String,Object>) row.get("value");
+                    assertTrue(result.get("created") instanceof Number);
+                    assertTrue(result.containsKey("choices"));
+                    var finishReason = (String)((List<Map>) result.get("choices")).get(0).get("finish_reason");
+                    assertTrue(finishReason.matches("stop|length"));
+                    assertEquals(MODEL_ID, result.get("model"));
+                    
+                    assertFalse(r.hasNext());
+                    return true;
+                }
+        ));
     }
 
     @Test
     public void chatCompletion() {
-        testCall(db, """
+        assertEventually(() -> db.executeTransactionally("""
             CALL apoc.ml.openai.chat([
             {role:"system", content:"Only answer with a single word"},
             {role:"user", content:"What planet do humans live on?"}
             ],  $apiKey, $conf)
             """, 
-                getParams("meta-llama/Llama-2-70b-chat-hf"),
-                (row) -> assertChatCompletion(row, "gpt-3.5-turbo"));
+                getParams(MODEL_ID),
+                (r) -> {
+                    Map<String, Object> row = r.next();
+                    var result = (Map<String,Object>) row.get("value");
+                    assertTrue(result.get("created") instanceof Number);
+                    assertTrue(result.containsKey("choices"));
+
+                    Map message = ((List<Map<String,Map>>) result.get("choices")).get(0).get("message");
+                    assertEquals("assistant", message.get("role"));
+                    String text = (String) message.get("content");
+                    assertTrue(text != null && !text.isBlank());
+                    assertTrue(result.get("model").toString().startsWith(MODEL_ID));
+                    
+                    assertFalse(r.hasNext());
+                    return true;
+                }
+        ));
+    }
+
+
+    private void assertEventually(Callable<Boolean> booleanCallable) {
+        Assert.assertEventually(() -> {
+            try {
+                return booleanCallable.call();
+            } catch (RuntimeException e) {
+                System.out.println("e = " + e);
+                return false;
+            }
+        }, val -> val, 60, TimeUnit.SECONDS);
     }
 
     private Map<String, Object> getParams(String model) {
--- extended/src/test/java/apoc/ml/OpenAIOpenLMIT.java
+++ extended/src/test/java/apoc/ml/OpenAIOpenLMIT.java
@@ -15,9 +15,12 @@ import static apoc.ml.OpenAI.ENDPOINT_CONF_KEY;
 import static apoc.ml.OpenAITestResultUtils.assertChatCompletion;
 import static apoc.ml.OpenAITestResultUtils.assertCompletion;
 import static apoc.util.TestUtil.testCall;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * OpenLM allows
+ * 
+ * It works only for `\completion` API
  */
 public class OpenAIOpenLMIT {
 
@@ -29,45 +32,57 @@ public class OpenAIOpenLMIT {
 
     @Before
     public void setUp() throws Exception {
-        openaiKey = System.getenv("OPENAI_KEY");
-        Assume.assumeNotNull("No OPENAI_KEY environment configured", openaiKey);
+        openaiKey = System.getenv("HF_API_TOKEN");
+        Assume.assumeNotNull("No HF_API_TOKEN environment configured", openaiKey);
         TestUtil.registerProcedure(db, OpenAI.class);
     }
 
-    @Test
-    public void getEmbedding() {
-        // https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2
-        
-        // TODO?? -- JsonUtil.loadJson(url, headers, payload, "$", true, List.of(), urlAccessChecker)??
-        testCall(db, "CALL apoc.ml.openai.embedding(['Some Text'], $apiKey, $conf)",
-                getParams("thenlper/gte-large"),
-                OpenAITestResultUtils::assertEmbeddings);
-    }
+//    @Test
+//    public void getEmbedding() {
+//        // https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2
+//        
+//        // TODO?? -- JsonUtil.loadJson(url, headers, payload, "$", true, List.of(), urlAccessChecker)??
+//        testCall(db, "CALL apoc.ml.openai.embedding(['Some Text'], $apiKey, $conf)",
+//                getParams("thenlper/gte-large"),
+//                OpenAITestResultUtils::assertEmbeddings);
+//    }
 
     @Test
     public void completion() {
+        String modelId = "gpt2";
+        Map<String, String> conf = Map.of(ENDPOINT_CONF_KEY, "https://api-inference.huggingface.co/models/" + modelId,
+                API_TYPE_CONF_KEY, OpenAIRequestHandler.Type.HUGGINGFACE.name()
+                ,
+                "model", modelId
+        );
         testCall(db, "CALL apoc.ml.openai.completion('What color is the sky? Answer in one word: ', $apiKey, $conf)",
-                getParams("Meta-Llama/Llama-Guard-7b"),
-                (row) -> assertCompletion(row, "gpt2"));
+                Map.of("conf", conf, "apiKey", openaiKey),
+//                getParams("Meta-Llama/Llama-Guard-7b"),
+                (row) -> {
+                    var result = (Map<String,Object>) row.get("value");
+                    String generatedText = (String) result.get("generated_text");
+                    assertTrue(generatedText.toLowerCase().contains("blue"),
+                            "Actual generatedText is " + generatedText);
+                });
     }
 
-    @Test
-    public void chatCompletion() {
-        testCall(db, """
-                        CALL apoc.ml.openai.chat([
-                                    {
-                                    	inputs: "non so"
-                                    }
-                        ],  $apiKey, $conf)
-                        """, 
-                getParams("meta-llama/Llama-2-70b-chat-hf"),
-                (row) -> assertChatCompletion(row, "gpt2"));
-    }
+//    @Test
+//    public void chatCompletion() {
+//        testCall(db, """
+//                        CALL apoc.ml.openai.chat([
+//                                    {
+//                                    	inputs: "non so"
+//                                    }
+//                        ],  $apiKey, $conf)
+//                        """, 
+//                getParams("meta-llama/Llama-2-70b-chat-hf"),
+//                (row) -> assertChatCompletion(row, "gpt2"));
+//    }
 
     private Map<String, Object> getParams(String model) {
-        return Map.of("apiKey", openaiKey,
-                "conf", Map.of(ENDPOINT_CONF_KEY, "https://api-inference.huggingface.co/models/gpt2",
-                        API_TYPE_CONF_KEY, OpenAIRequestHandler.Type.HUGGING_FACE.name(),
+        return Map.of(//"apiKey", openaiKey,
+                "conf", Map.of(//ENDPOINT_CONF_KEY, "https://api-inference.huggingface.co/models/gpt2",
+                        API_TYPE_CONF_KEY, OpenAIRequestHandler.Type.HUGGINGFACE.name(),
                         "model", model
                 )
         );
