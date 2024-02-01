d68ffbb8118608f5e2f50a12be876e7fcb01b7fb
vga91 <giuseppe.villani@larus-ba.it>
2024-02-01 16:11:22 +0100

    wip

--- extended/src/main/java/apoc/agg/MultiStats.java
+++ extended/src/main/java/apoc/agg/MultiStats.java
@@ -1,13 +1,18 @@
 package apoc.agg;
 
 import apoc.Extended;
+import com.amazonaws.util.NumberUtils;
 import org.HdrHistogram.HistogramUtil;
 import org.neo4j.graphdb.Entity;
+import org.neo4j.kernel.impl.util.ValueUtils;
 import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.UserAggregationFunction;
 import org.neo4j.procedure.UserAggregationResult;
 import org.neo4j.procedure.UserAggregationUpdate;
+import org.neo4j.values.storable.LongValue;
+import org.neo4j.values.storable.NumberValue;
+import org.neo4j.values.utils.ValueMath;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -54,30 +59,69 @@ https://community.neo4j.com/t/listing-the-community-size-of-different-community-
 //        private DoubleHistogram doubles;
 //        private List<Double> percentiles = asList(0.5D, 0.75D, 0.9D, 0.95D, 0.9D, 0.99D);
 //        private Number minValue;
-        private Map<String, Map<String, Number>> result;
+        private final Map<String, Map<String, NumberValue>> result = new HashMap<>();
 
+        // --> TODO - sum must be similar to https://neo4j.com/docs/cypher-manual/current/functions/aggregating/#functions-sum
+        
         @UserAggregationUpdate
         public void aggregate(
-                @Name("value") Entity value,
+                @Name("value") Object value,
                 @Name(value = "keys") List<String> keys,
                 // todo...
                 @Name(value = "statistics", defaultValue = "['sum','count','avg']") List<String> statistics
                 ) {
+            // todo - can be also a map, maybe?
+            Entity entity = (Entity) value; 
+            
             // per ogni prop
             keys.forEach(key -> {
-                if (value.hasProperty(key)) {
-                    Object property = value.getProperty(key);
+                if (entity.hasProperty(key)) {
+                    Object property = entity.getProperty(key);
                     // todo - forse metterlo all'esterno
-                    Map<String, Number> orDefault = result.getOrDefault(key, new HashMap<>());
-                    orDefault.compute("count", 
+                    result.compute(key, (ignored, v) -> {
+                        Map<String, NumberValue> map;
+                        if (v == null) {
+                            map = new HashMap<>();
+                        } else {
+                            map = v;
+                        }
+
+                        NumberValue count = map.compute("count", ((subKey, subVal) -> (NumberValue) ValueUtils.of(subVal == null ? 1 : subVal.longValue() + 1)) );
+                        
+                        if (property instanceof Number propNum) {
+//                            NumberValue
+//                            
+                            NumberValue of = (NumberValue) ValueUtils.of(property);
+                            
+//                            ValueMath.overflowSafeAdd(
+                            
+//                            ValueMath.overflowSafeAdd(  )
+                            
+                            
+                            // todo - double and long must be different ?
+                            NumberValue sum = map.compute("sum", ((subKey, subVal) -> subVal == null ? of : ValueMath.overflowSafeAdd(subVal, of)));
+
+                            // NB: avg() return always a double
+                            NumberValue avg = map.compute("avg", ((subKey, subVal) -> subVal == null ? of : sum.dividedBy(count.doubleValue())  ));
+//                            NumberValue avg = map.compute("avg", ((subKey, subVal) -> subVal == null ? of : sum.divideBy (count)  ));
+                        }
+
+                        return map;
+                    });
+                    
+//                    Map<String, Number> orDefault = result.getOrDefault(key, new HashMap<>());
+//                    orDefault.compute("count", ((k, v) -> v == null ? 1 : v.longValue() + 1));
+//                    System.out.println("orDefault = " + orDefault);
                 }
             });
 //            value.getProperty()
         }
+        
+        // --> Map<key, Map<key, Map<key, value>> >
 
         @UserAggregationResult
         // apoc.agg.multiStats([key1,key2,key3]) -> Map<Key,Map<agg="sum,count,avg", number>>
-        public Map<String, Map<String, Number>> result() {
+        public Map<String, Map<String, NumberValue>> result() {
             return result;
         }
     }
--- extended/src/test/java/apoc/agg/MultiStatsTest.java
+++ extended/src/test/java/apoc/agg/MultiStatsTest.java
@@ -3,6 +3,7 @@ package apoc.agg;
 import apoc.map.Maps;
 import apoc.util.TestUtil;
 import apoc.util.Util;
+import apoc.util.collection.Iterators;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
@@ -14,6 +15,10 @@ import org.neo4j.test.rule.ImpermanentDbmsRule;
 
 import java.util.Map;
 
+import static apoc.util.TestUtil.firstColumn;
+import static apoc.util.TestUtil.testCall;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MultiStatsTest {
 
     @ClassRule
@@ -21,7 +26,7 @@ public class MultiStatsTest {
 
     @BeforeClass
     public static void setUp() {
-        TestUtil.registerProcedure(db, MaxAndMinItems.class, Maps.class);
+        TestUtil.registerProcedure(db, Maps.class, MultiStats.class);
         
         db.executeTransactionally("""
                 CREATE (n:Person {
@@ -29,7 +34,7 @@ public class MultiStatsTest {
                     neo4jImportId: "18349390",
                     wcc: 48,
                     lpa: 598,
-                    naam: "Jansen, J",
+                    name: "aaa",
                     wcc_cypher: 548
                 });""");
         db.executeTransactionally("""
@@ -38,7 +43,7 @@ public class MultiStatsTest {
                     neo4jImportId: "18349390",
                     wcc: 48,
                     lpa: 598,
-                    naam: "Jansen, J",
+                    name: "eee",
                     wcc_cypher: 549
                 });""");
         db.executeTransactionally("""
@@ -47,9 +52,35 @@ public class MultiStatsTest {
                     neo4jImportId: "18349390",
                     wcc: 47,
                     lpa: 596,
-                    naam: "Jansen, J",
+                    name: "iii",
                     wcc_cypher: 549
                 })""");
+    }
+
+    @Test
+    public void test2() {
+        db.executeTransactionally("CREATE (:Test {a: 1, b: 2.0, c: 3.0}), " +
+                                  "(:Test {a: 2, b: 2.0, c: 3.5}), " +
+                                  "(:Test {a: 3, b: 3, c: 4.5})");
+
+        Map<String, Object> stringObjectMap1 = db.executeTransactionally(
+                "MATCH (n:Test) WITH n.a as prop return sum(prop) AS sum, avg(prop) AS avg, count(prop) AS count", Map.of(),
+                Iterators::single);
+        
+        Map<String, Object> stringObjectMap2 = db.executeTransactionally(
+                "MATCH (n:Test) WITH n.b as prop return sum(prop) AS sum, avg(prop) AS avg, count(prop) AS count", Map.of(),
+                Iterators::single);
+        
+        Map<String, Object> stringObjectMap3 = db.executeTransactionally(
+                "MATCH (n:Test) WITH n.c as prop return sum(prop) AS sum, avg(prop) AS avg, count(prop) AS count", Map.of(),
+                Iterators::single);
+
+        testCall(db, "MATCH (n:Test) RETURN apoc.agg.multiStats(n, ['a', 'b', 'c']) AS data", r -> {
+            Map data = (Map) r.get("data");
+            assertEquals(stringObjectMap1, data.get("a"));
+            assertEquals(stringObjectMap2, data.get("b"));
+            assertEquals(stringObjectMap3, data.get("c"));
+        });
     }
 
     @AfterClass
@@ -82,11 +113,11 @@ public class MultiStatsTest {
                             MATCH (lpa:Person {lpa: p.lpa})
                             RETURN count(*) AS lpa
                         }
-                        RETURN p.naam, lpa,  wcc, wcc_cypher, louvain
+                        RETURN p.name, lpa,  wcc, wcc_cypher, louvain
                         """, Map.of(),
                 Result::resultAsString);
         System.out.println("s = " + s);
-
+        
 
 
         String s1 = db.executeTransactionally("""
@@ -100,16 +131,28 @@ public class MultiStatsTest {
                         WITH map_lpa, map_wcc, p.louvain as louvain, count(*) as sizeLouvain
                         WITH map_lpa, map_wcc, apoc.map.fromPairs(collect([toString(louvain), sizeLouvain])) as map_louvain
                         MATCH (p:Person)
-                        RETURN p.naam, map_lpa[toString(p.lpa)] as lpaSize,  map_wcc[toString(p.wcc)] as wccSize, map_louvain[toString(p.louvain)] as wccLouvain""",
+                        RETURN p.name, map_lpa[toString(p.lpa)] as lpaSize,  map_wcc[toString(p.wcc)] as wccSize, map_louvain[toString(p.louvain)] as louvainSize""",
                 Map.of(), Result::resultAsString);
         System.out.println("s1 = " + s1);
 
 
+//        String s2 = db.executeTransactionally("""
+//                match (p:Person)
+//                WITH apoc.agg.multiStats(p, ["lpa","wcc","louvain"], ["count"]) as data
+//                MATCH (p:Person)
+//                RETURN p.name, data.lpa.count as lpaSize, data.wcc.count as wccSize, data.louvain.count as louvainSize
+//                """, Map.of(), Result::resultAsString);
+//        System.out.println("s2 = " + s2);
         String s2 = db.executeTransactionally("""
                 match (p:Person)
-                with apoc.agg.multiStats(p, ["wcc","lpa","louvain"]) as data
-                return p.name, data[toString(p.wcc)].count as size
+                RETURN apoc.agg.multiStats(p, ["lpa","wcc","louvain"], ["count"]) as data
+                match (p:Person)
+                return p.name, data.wcc.[toString(p.wcc)].count as size
+                //MATCH (p:Person)
+                //RETURN p.name, data.lpa.count as lpaSize, data.wcc.count as wccSize, data.louvain.count as louvainSize
                 """, Map.of(), Result::resultAsString);
         System.out.println("s2 = " + s2);
+        
+        // todo...
     }
 }
