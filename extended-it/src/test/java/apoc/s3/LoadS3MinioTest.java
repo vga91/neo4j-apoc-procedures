2427060852e8fbd3900d76675c39746b3b3a44fa
vga91 <giuseppe.villani@larus-ba.it>
2025-02-05 13:01:43 +0100

    cleanup

--- extended-it/src/test/java/apoc/s3/LoadS3MinioTest.java
+++ extended-it/src/test/java/apoc/s3/LoadS3MinioTest.java
@@ -7,29 +7,36 @@ import apoc.load.LoadCsv;
 import apoc.load.LoadJson;
 import apoc.load.Xml;
 import apoc.util.TestUtil;
-import org.junit.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.neo4j.graphdb.ResourceIterator;
 import org.neo4j.test.rule.DbmsRule;
 import org.neo4j.test.rule.ImpermanentDbmsRule;
-
-import static apoc.ApocConfig.*;
-import static apoc.util.MapUtil.map;
-
-
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
 import java.time.Duration;
+import java.util.Map;
+
+import static apoc.ApocConfig.*;
+import static apoc.util.MapUtil.map;
+import static apoc.util.TestUtil.testCall;
+import static apoc.util.TestUtil.testResult;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class LoadS3MinioTest {
 
-    static final String ACCESS_KEY = "testAccessKey";
-    static final String SECRET_KEY = "testSecretKey";
-    static final String BUCKET_NAME = "test";
-    static GenericContainer<?> minioContainer;
+    private static final String ACCESS_KEY = "testAccessKey";
+    private static final String SECRET_KEY = "testSecretKey";
+    private static final String BUCKET_NAME = "test";
+    private static GenericContainer<?> minioContainer;
 
     @ClassRule
     public static DbmsRule db = new ImpermanentDbmsRule();
-
+    
     @BeforeClass
     public static void init() throws Throwable {
         // to make Minio work
@@ -43,28 +50,19 @@ public class LoadS3MinioTest {
         apocConfig().setProperty(APOC_EXPORT_FILE_ENABLED, true);
         apocConfig().setProperty(APOC_IMPORT_FILE_USE_NEO4J_CONFIG, false);
 
-        db.executeTransactionally(
-                "CREATE (f:User1:User {name:'foo'})-[:KNOWS]->(b:User {name:'bar'})");
+        db.executeTransactionally("CREATE (f:User1:User {name:'foo'})-[:KNOWS]->(b:User {name:'bar'})");
 
         minioContainer = new GenericContainer<>("bitnami/minio:2025.1.20")
-        //minioContainer = new GenericContainer<>("bitnami/minio:2025.1.20")
                 .withExposedPorts(9000, 9001)
                 .withEnv("MINIO_ROOT_USER", ACCESS_KEY)
                 .withEnv("MINIO_ROOT_PASSWORD", SECRET_KEY)
                 .withEnv("MINIO_DEFAULT_BUCKETS", BUCKET_NAME)
-                .waitingFor(Wait.forHttp("/").forStatusCode(200));
-
-       // TODO - NON FUNZIONA
-        minioContainer.setWaitStrategy(
-                Wait.forLogMessage(".*Bucket created successfully.*\\n", 1)
-                        .withStartupTimeout(Duration.ofSeconds(30))
-        );
-            //   minioContainer.withCommand("server", "/data");
+                .waitingFor(
+                        Wait.forLogMessage(".*Bucket created successfully.*\\n", 1)
+                                .withStartupTimeout(Duration.ofSeconds(30))
+                );
 
         minioContainer.start();
-
-      //  assertEventually(() -> minioContainer.getLogs().contains("Bucket created successfully"),
-      //          val -> val, 20L, TimeUnit.SECONDS);
     }
 
     @AfterClass
@@ -87,74 +85,68 @@ public class LoadS3MinioTest {
 
     @Test
     public void testLoadCsvS3() throws Exception {
-
-//        Thread.sleep(5000);
-
+        
         String url = getUrl("test.csv");
 
-        String url12 = db.executeTransactionally("CALL apoc.export.csv.all($url,{failOnError:true})",
+        testCall(db, "CALL apoc.export.csv.all($url, {})",
                 map("url", url),
-                org.neo4j.graphdb.Result::resultAsString);
-        System.out.println("url12 = " + url12);
+                r -> assertEquals(url, r.get("file"))
+        );
 
-        String url1 = db.executeTransactionally("CALL apoc.load.csv($url,{failOnError:true})",
+        testResult(db, "CALL apoc.load.csv($url)",
                 map("url", url),
-                org.neo4j.graphdb.Result::resultAsString);
-        System.out.println("url1 = " + url1);
-        System.out.println("LoadS3MinioTest.testLoadCsvS3");
-        // --> Failed to invoke procedure `apoc.load.csv`: Caused by: javax.net.ssl.SSLException: Unsupported or unrecognized SSL message
-
-//        String url = minio.putFile("../extended/src/test/resources/test.csv");
-//        testResult(db, "CALL apoc.load.csv($url,{failOnError:false})", map("url", url), (r) -> {
-//            assertRow(r, "Selma", "8", 0L);
-//            assertRow(r, "Rana", "11", 1L);
-//            assertRow(r, "Selina", "18", 2L);
-//            assertEquals(false, r.hasNext());
-//        });
+                r -> {
+                    ResourceIterator<Map> values = r.columnAs("map");
+                    Map row = values.next();
+                    assertEquals(":User:User1", row.get("_labels"));
+                    row = values.next();
+                    assertEquals(":User", row.get("_labels"));
+                    row = values.next();
+                    assertEquals("KNOWS", row.get("_type"));
+                    assertFalse(values.hasNext());
+                });
     }
 
     @Test
     public void testLoadJsonS3() throws Exception {
-        //String url = minio.putFile("../extended/src/test/resources/map.json");
-
         String url = getUrl("test.json");
 
-        String url12 = db.executeTransactionally("CALL apoc.export.json.all($url,{failOnError:true})",
+        testCall(db, "CALL apoc.export.json.all($url)",
                 map("url", url),
-                org.neo4j.graphdb.Result::resultAsString);
-        System.out.println("url12 = " + url12);
-
-        String url1 = db.executeTransactionally("CALL apoc.load.json($url,'')",
+                r -> assertEquals(url, r.get("file"))
+        );
+        
+        testResult(db, "CALL apoc.load.json($url,'')",
                 map("url", url),
-                org.neo4j.graphdb.Result::resultAsString);
-        System.out.println("url1 = " + url1);
-        System.out.println("LoadS3MinioTest.testLoadCsvS3");
-
-    /*    testCall(db, "CALL apoc.load.json($url,'')",
-                map("url", url),
-                (row) -> {
-                    System.out.println("row = " + row);
-                    assertEquals(map("foo",asList(1L,2L,3L)), row.get("value"));
+                r -> {
+                    ResourceIterator<Map> values = r.columnAs("value");
+                    Map row = values.next();
+                    assertEquals("node", row.get("type"));
+                    row = values.next();
+                    assertEquals("node", row.get("type"));
+                    row = values.next();
+                    assertEquals("relationship", row.get("type"));
+                    assertFalse(values.hasNext());
                 });
-
-     */
     }
 
     @Test
     public void testLoadXmlS3() throws Exception {
-        //String url = minio.putFile("../extended/src/test/resources/xml/books.xml");
         String url = getUrl("test.xml");
 
-        String url12 = db.executeTransactionally("CALL apoc.export.graphml.all($url,{failOnError:true})",
+        testCall(db, "CALL apoc.export.graphml.all($url, {})",
                 map("url", url),
-                org.neo4j.graphdb.Result::resultAsString);
-        System.out.println("url12 = " + url12);
+                r -> assertEquals(url, r.get("file"))
+        );
 
-        String url1 = db.executeTransactionally("CALL apoc.load.xml($url,'')",
+        testResult(db, "CALL apoc.load.xml($url,'')",
                 map("url", url),
-                org.neo4j.graphdb.Result::resultAsString);
-        System.out.println("url1 = " + url1);
-        System.out.println("LoadS3MinioTest.testLoadCsvS3");
+                r -> {
+                    ResourceIterator<Map> values = r.columnAs("value");
+                    Map row = values.next();
+                    assertEquals("graphml", row.get("_type"));
+                    assertFalse(values.hasNext());
+                });
     }
 
 
