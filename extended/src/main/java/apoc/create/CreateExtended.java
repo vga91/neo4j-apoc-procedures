ebec19bb3793f3abdeaedcdf036c9d838da0d984
vga91 <giuseppe.villani@larus-ba.it>
2025-02-26 18:04:46 +0100

    Fixes 2181: extend apoc.create.virtual.fromNode(node,[propertyNames]) to add new virtual properties

---
+++ extended/src/main/java/apoc/create/CreateExtended.java
@@ -0,0 +1,39 @@
+package apoc.create;
+
+import apoc.Extended;
+import apoc.result.VirtualNode;
+import apoc.util.Util;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.procedure.Context;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.UserFunction;
+
+import java.util.List;
+import java.util.Map;
+
+@Extended
+public class CreateExtended {
+
+    
+    // TODO - tests and docs
+    
+    @Context
+    public Transaction tx;
+
+    @UserFunction("apoc.create.virtual.fromNodeExtended")
+    @Description(
+            "Returns a virtual `NODE` from the given existing `NODE`. The virtual `NODE` only contains the requested properties.")
+    public Node virtualFromNodeFunction(
+            @Name(value = "node", description = "The node to generate a virtual node from.") Node node,
+            @Name(value = "propertyNames", description = "The properties to copy to the virtual node.") List<String> propertyNames,
+            @Name(value = "additionalProperties", defaultValue = "{}") Map<String, Object> additionalProperties,
+            @Name(value = "config", defaultValue = "{}", description = "{ wrapNodeIds = false :: BOOLEAN }")
+            Map<String, Object> config) {
+        boolean wrapNodeIds = Util.toBoolean(config.get("wrapNodeIds"));
+        VirtualNode virtualNode = new VirtualNode(node, propertyNames, wrapNodeIds);
+        additionalProperties.forEach(virtualNode::setProperty);
+        return virtualNode;
+    }
+}
