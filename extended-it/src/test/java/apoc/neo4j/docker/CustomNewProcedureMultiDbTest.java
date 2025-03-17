b661065f2c702907cc47db6e1a7b7642b5d3ae04
vga91 <giuseppe.villani@larus-ba.it>
2025-03-18 00:34:02 +0100

    added test

--- extended-it/src/test/java/apoc/neo4j/docker/CustomNewProcedureMultiDbTest.java
+++ extended-it/src/test/java/apoc/neo4j/docker/CustomNewProcedureMultiDbTest.java
@@ -5,6 +5,7 @@ import apoc.util.TestContainerUtil;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.Record;
 import org.neo4j.driver.Session;
@@ -14,31 +15,30 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-import static apoc.util.TestContainerUtil.createEnterpriseDB;
+import static apoc.util.ExtendedTestContainerUtil.singleResultFirstColumn;
+import static apoc.util.TestContainerUtil.*;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.Assert.*;
+import static org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATABASE_NAME;
+import static org.neo4j.configuration.GraphDatabaseSettings.SYSTEM_DATABASE_NAME;
 import static org.neo4j.test.assertion.Assert.assertEventually;
 
 public class CustomNewProcedureMultiDbTest {
 
+    public static final long TIMEOUT = 60L;
     private static Neo4jContainerExtension neo4jContainer;
     private static Driver driver;
     private static Session neo4jSession;
     private static Session testSession;
     private static Session fooSession;
-    private static Session barSession;
+    private static Session systemSession;
 
     private static final String DB_TEST = "dbtest";
     private static final String DB_FOO = "dbfoo";
-    private static final String DB_BAR = "dbbar";
 
     @BeforeClass
     public static void setupContainer() {
-        neo4jContainer = createEnterpriseDB(List.of(TestContainerUtil.ApocPackage.EXTENDED), true)
-                .withEnv(Map.of("apoc.ttl.enabled." + DB_TEST, "false",
-                        "apoc.ttl.enabled", "true",
-                        "apoc.ttl.schedule", "2",
-                        "apoc.ttl.schedule." + DB_FOO, "7",
-                        "apoc.ttl.limit", "200",
-                        "apoc.ttl.limit." + DB_BAR, "2000"));
+        neo4jContainer = createEnterpriseDB(List.of(TestContainerUtil.ApocPackage.EXTENDED), true);
         neo4jContainer.start();
         driver = neo4jContainer.getDriver();
         createDatabases();
@@ -50,7 +50,6 @@ public class CustomNewProcedureMultiDbTest {
         neo4jSession.executeWrite(tx -> tx.run("MATCH (n) DETACH DELETE n;").consume());
         testSession.executeWrite(tx -> tx.run("MATCH (n) DETACH DELETE n;").consume());
         fooSession.executeWrite(tx -> tx.run("MATCH (n) DETACH DELETE n;").consume());
-        barSession.executeWrite(tx -> tx.run("MATCH (n) DETACH DELETE n;").consume());
     }
 
     @AfterClass
@@ -59,14 +58,94 @@ public class CustomNewProcedureMultiDbTest {
     }
 
     
-    // TODO - like TTLMultiDbTest
+    @Test
+    public void testProceduresFunctionsInMultipleDatabase() throws Exception {
+        String countCustom = "CALL apoc.custom.list() YIELD name RETURN count(*) AS count";
+        
+        systemSession.executeWrite(tx -> 
+                tx.run("CALL apoc.custom.installProcedure('neo4jProc() :: (answer::INT)','RETURN 42 as answer', $db)", 
+                        Map.of("db", DEFAULT_DATABASE_NAME)).consume()
+        );
+        systemSession.executeWrite(tx -> 
+                tx.run("CALL apoc.custom.installProcedure('fooProc() :: (answer::INT)','RETURN 42 as answer', $db)",
+                        Map.of("db", DB_FOO)).consume()
+        );
+        systemSession.executeWrite(tx -> 
+                tx.run("CALL apoc.custom.installProcedure('testProc() :: (answer::INT)','RETURN 42 as answer', $db)",
+                        Map.of("db", DB_TEST)).consume()
+        );
+
+        systemSession.executeWrite(tx ->
+                tx.run("CALL apoc.custom.installFunction('neo4jFun() :: INT','RETURN 42 as answer', $db)",
+                        Map.of("db", DEFAULT_DATABASE_NAME)).consume()
+        );
+        systemSession.executeWrite(tx ->
+                tx.run("CALL apoc.custom.installFunction('fooFun() :: INT','RETURN 42 as answer', $db)",
+                        Map.of("db", DB_FOO)).consume()
+        );
+        systemSession.executeWrite(tx ->
+                tx.run("CALL apoc.custom.installFunction('testFun() :: INT','RETURN 42 as answer', $db)",
+                        Map.of("db", DB_TEST)).consume()
+        );
+
+        assertEventually(() -> (long) singleResultFirstColumn(neo4jSession, countCustom),
+                (value) -> value == 2L, TIMEOUT, SECONDS);
+        
+        assertEventually(() -> (long) singleResultFirstColumn(fooSession, countCustom),
+                (value) -> value == 2L, TIMEOUT, SECONDS);
+        
+        assertEventually(() -> (long) singleResultFirstColumn(testSession, countCustom),
+                (value) -> value == 2L, TIMEOUT, SECONDS);
+
+
+        neo4jSession.executeWrite(tx->tx.run("call db.clearQueryCaches()").consume());
+        fooSession.executeWrite(tx->tx.run("call db.clearQueryCaches()").consume());
+        testSession.executeWrite(tx->tx.run("call db.clearQueryCaches()").consume());
+        
+        
+        String neo4jProc = "CALL custom.%s".formatted("neo4jProc");
+        String neo4jFun = "RETURN custom.%s() AS answer".formatted("neo4jFun");
+        assertEventually(() -> {
+                    try {
+                        long res = singleResultFirstColumn(neo4jSession, neo4jProc);
+                        return 42L == res;
+                    } catch (Exception e) {
+                        return false;
+                    }
+                },
+                (v) -> v, TIMEOUT, SECONDS);
+
+        assertEventually(() -> {
+                    try {
+                        long res = singleResultFirstColumn(neo4jSession, neo4jFun);
+                        return 42L == res;
+                    } catch (Exception e) {
+                        return false;
+                    }
+                },
+                (v) -> v, TIMEOUT, SECONDS);
+
+        try {
+            testCall(neo4jSession, neo4jProc, r -> fail("Should fail due to not existing procedure "));
+        } catch (Exception e) {
+            String actualErrMsg = e.getMessage();
+            assertTrue("Actual err. message is: " + actualErrMsg, actualErrMsg.contains("There is no procedure with the name"));
+        }
+        
+        try {
+            testCall(neo4jSession, neo4jProc, r -> fail("Should fail due to not existing function"));
+        } catch (Exception e) {
+            String actualErrMsg = e.getMessage();
+            assertTrue("Actual err. message is: " + actualErrMsg, actualErrMsg.contains("Unknown function"));
+        }
+    }
 
     private static void createDatabases() {
         try(Session systemSession = driver.session(SessionConfig.forDatabase("system"))) {
             systemSession.executeWrite(tx -> {
                 tx.run("CREATE DATABASE " + DB_TEST + " WAIT;").consume();
                 tx.run("CREATE DATABASE " + DB_FOO + " WAIT;").consume();
-                return tx.run("CREATE DATABASE " + DB_BAR + " WAIT;").consume();
+                return null;
             });
         }
 
@@ -75,16 +154,16 @@ public class CustomNewProcedureMultiDbTest {
                 final List<Record> list = systemSession.run("SHOW DATABASES YIELD name, currentStatus")
                         .list();
                 return list.stream().allMatch(i -> i.get("currentStatus").asString().equals("online"))
-                        && list.stream().map(i -> i.get("name").asString()).toList().containsAll(List.of(DB_TEST , DB_FOO , DB_BAR));
+                        && list.stream().map(i -> i.get("name").asString()).toList().containsAll(List.of(DB_TEST , DB_FOO));
             }, value -> value, 30L, TimeUnit.SECONDS);
         }
     }
 
     private static void createSessions() {
         neo4jSession = neo4jContainer.getSession();
+        systemSession = driver.session(SessionConfig.forDatabase(SYSTEM_DATABASE_NAME));
         testSession = driver.session(SessionConfig.forDatabase(DB_TEST));
         fooSession = driver.session(SessionConfig.forDatabase(DB_FOO));
-        barSession = driver.session(SessionConfig.forDatabase(DB_BAR));
     }
 
 }
