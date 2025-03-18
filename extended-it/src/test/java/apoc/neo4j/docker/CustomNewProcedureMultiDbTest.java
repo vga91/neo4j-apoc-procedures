c61522792823c51c0c408b0db62d941ab5e501ab
vga91 <giuseppe.villani@larus-ba.it>
2025-03-18 01:27:18 +0100

    added tests

--- extended-it/src/test/java/apoc/neo4j/docker/CustomNewProcedureMultiDbTest.java
+++ extended-it/src/test/java/apoc/neo4j/docker/CustomNewProcedureMultiDbTest.java
@@ -56,38 +56,17 @@ public class CustomNewProcedureMultiDbTest {
     public static void bringDownContainer() {
         neo4jContainer.close();
     }
-
     
     @Test
-    public void testProceduresFunctionsInMultipleDatabase() throws Exception {
+    public void testProceduresFunctionsInMultipleDatabase() {
+
+        // install a procedure and a function for each database
+        installNeo4jProcAndFun();
+        installFooProcAndFun();
+        installTestProcAndFun();
+
+        // check that every database has 2 custom procedures/functions
         String countCustom = "CALL apoc.custom.list() YIELD name RETURN count(*) AS count";
-        
-        systemSession.executeWrite(tx -> 
-                tx.run("CALL apoc.custom.installProcedure('neo4jProc() :: (answer::INT)','RETURN 42 as answer', $db)", 
-                        Map.of("db", DEFAULT_DATABASE_NAME)).consume()
-        );
-        systemSession.executeWrite(tx -> 
-                tx.run("CALL apoc.custom.installProcedure('fooProc() :: (answer::INT)','RETURN 42 as answer', $db)",
-                        Map.of("db", DB_FOO)).consume()
-        );
-        systemSession.executeWrite(tx -> 
-                tx.run("CALL apoc.custom.installProcedure('testProc() :: (answer::INT)','RETURN 42 as answer', $db)",
-                        Map.of("db", DB_TEST)).consume()
-        );
-
-        systemSession.executeWrite(tx ->
-                tx.run("CALL apoc.custom.installFunction('neo4jFun() :: INT','RETURN 42 as answer', $db)",
-                        Map.of("db", DEFAULT_DATABASE_NAME)).consume()
-        );
-        systemSession.executeWrite(tx ->
-                tx.run("CALL apoc.custom.installFunction('fooFun() :: INT','RETURN 42 as answer', $db)",
-                        Map.of("db", DB_FOO)).consume()
-        );
-        systemSession.executeWrite(tx ->
-                tx.run("CALL apoc.custom.installFunction('testFun() :: INT','RETURN 42 as answer', $db)",
-                        Map.of("db", DB_TEST)).consume()
-        );
-
         assertEventually(() -> (long) singleResultFirstColumn(neo4jSession, countCustom),
                 (value) -> value == 2L, TIMEOUT, SECONDS);
         
@@ -97,43 +76,89 @@ public class CustomNewProcedureMultiDbTest {
         assertEventually(() -> (long) singleResultFirstColumn(testSession, countCustom),
                 (value) -> value == 2L, TIMEOUT, SECONDS);
 
-
+        // clear caches
         neo4jSession.executeWrite(tx->tx.run("call db.clearQueryCaches()").consume());
         fooSession.executeWrite(tx->tx.run("call db.clearQueryCaches()").consume());
         testSession.executeWrite(tx->tx.run("call db.clearQueryCaches()").consume());
         
+        // check that each procedure and function is installed only in the specified db
+        chackThatFunAndProcAreInstalledOnlyInTheSpecifiedDb(neo4jSession, 
+                "CALL custom.neo4jProc", "RETURN custom.neo4jFun() AS answer",
+                fooSession, testSession);
+
+        chackThatFunAndProcAreInstalledOnlyInTheSpecifiedDb(fooSession, 
+                "CALL custom.fooProc", "RETURN custom.fooFun() AS answer",
+                neo4jSession, testSession);
         
-        String neo4jProc = "CALL custom.%s".formatted("neo4jProc");
-        String neo4jFun = "RETURN custom.%s() AS answer".formatted("neo4jFun");
+        chackThatFunAndProcAreInstalledOnlyInTheSpecifiedDb(testSession, 
+                "CALL custom.testProc", "RETURN custom.testFun() AS answer",
+                fooSession, neo4jSession);
+    }
+
+    private static void installTestProcAndFun() {
+        systemSession.executeWrite(tx -> 
+                tx.run("CALL apoc.custom.installProcedure('testProc() :: (answer::INT)','RETURN 42 as answer', $db)",
+                        Map.of("db", DB_TEST)).consume()
+        );
+        systemSession.executeWrite(tx ->
+                tx.run("CALL apoc.custom.installFunction('testFun() :: INT','RETURN 42 as answer', $db)",
+                        Map.of("db", DB_TEST)).consume()
+        );
+    }
+
+    private static void installFooProcAndFun() {
+        systemSession.executeWrite(tx -> 
+                tx.run("CALL apoc.custom.installProcedure('fooProc() :: (answer::INT)','RETURN 42 as answer', $db)",
+                        Map.of("db", DB_FOO)).consume()
+        );
+        systemSession.executeWrite(tx ->
+                tx.run("CALL apoc.custom.installFunction('fooFun() :: INT','RETURN 42 as answer', $db)",
+                        Map.of("db", DB_FOO)).consume()
+        );
+    }
+
+    private static void installNeo4jProcAndFun() {
+        systemSession.executeWrite(tx -> 
+                tx.run("CALL apoc.custom.installProcedure('neo4jProc() :: (answer::INT)','RETURN 42 as answer', $db)", 
+                        Map.of("db", DEFAULT_DATABASE_NAME)).consume()
+        );
+        systemSession.executeWrite(tx ->
+                tx.run("CALL apoc.custom.installFunction('neo4jFun() :: INT','RETURN 42 as answer', $db)",
+                        Map.of("db", DEFAULT_DATABASE_NAME)).consume()
+        );
+    }
+
+    private static void chackThatFunAndProcAreInstalledOnlyInTheSpecifiedDb(Session sessionWithCustomInstalled, String neo4jProcedure, String neo4jFunction, Session otherSessionDb, Session otherSessionDb2) {
+        checkInstalled(sessionWithCustomInstalled, neo4jProcedure);
+        checkInstalled(sessionWithCustomInstalled, neo4jFunction);
+
+        checkCustomProcAndFunNotInstalled(neo4jProcedure, neo4jFunction, otherSessionDb);
+
+        checkCustomProcAndFunNotInstalled(neo4jProcedure, neo4jFunction, otherSessionDb2);
+    }
+
+    private static void checkInstalled(Session sessionWithCustom, String neo4jProc) {
         assertEventually(() -> {
                     try {
-                        long res = singleResultFirstColumn(neo4jSession, neo4jProc);
+                        long res = singleResultFirstColumn(sessionWithCustom, neo4jProc);
                         return 42L == res;
                     } catch (Exception e) {
                         return false;
                     }
                 },
                 (v) -> v, TIMEOUT, SECONDS);
+    }
 
-        assertEventually(() -> {
-                    try {
-                        long res = singleResultFirstColumn(neo4jSession, neo4jFun);
-                        return 42L == res;
-                    } catch (Exception e) {
-                        return false;
-                    }
-                },
-                (v) -> v, TIMEOUT, SECONDS);
-
+    private static void checkCustomProcAndFunNotInstalled(String neo4jProc, String neo4jFun, Session otherSessionDb) {
         try {
-            testCall(neo4jSession, neo4jProc, r -> fail("Should fail due to not existing procedure "));
+            testCall(otherSessionDb, neo4jProc, r -> fail("Should fail due to not existing procedure "));
         } catch (Exception e) {
             String actualErrMsg = e.getMessage();
             assertTrue("Actual err. message is: " + actualErrMsg, actualErrMsg.contains("There is no procedure with the name"));
         }
-        
+
         try {
-            testCall(neo4jSession, neo4jProc, r -> fail("Should fail due to not existing function"));
+            testCall(otherSessionDb, neo4jFun, r -> fail("Should fail due to not existing function"));
         } catch (Exception e) {
             String actualErrMsg = e.getMessage();
             assertTrue("Actual err. message is: " + actualErrMsg, actualErrMsg.contains("Unknown function"));
